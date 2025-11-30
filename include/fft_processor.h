e7e45ed5af40ec32cf5d681d4b2114d61d88b61e
Chris Kai Frederick <git@neofin.net>
2025-11-29 16:20:21 -0800

    Fix doxygen comments

--- include/fft_processor.h
+++ include/fft_processor.h
@@ -7,16 +7,22 @@ struct fftwf_plan_s;
 using fftwf_plan = fftwf_plan_s*;
 using fftwf_complex = float[2];
 
-// @brief Processes audio samples using FFT to produce frequency spectrum
+/**
+ * @brief Processes audio samples using FFT to produce frequency spectrum
+ */
 class FFTProcessor
 {
   public:
-    // @brief Constructor
-    // @param num_bins Number of frequency bins for the FFT (must be power of 2)
-    // @throws std::invalid_argument if num_bins is not a power of 2
+    /**
+     * @brief Constructor
+     * @param num_bins Number of frequency bins for the FFT (must be power of 2)
+     * @throws std::invalid_argument if num_bins is not a power of 2
+     */
     explicit FFTProcessor(uint32_t num_bins);
 
-    // @brief Destructor
+    /**
+     * @brief Destructor
+     */
     ~FFTProcessor() = default;
 
     // Rule of five
@@ -25,18 +31,26 @@ class FFTProcessor
     FFTProcessor(FFTProcessor&&) = default;
     FFTProcessor& operator=(FFTProcessor&&) = default;
 
-    // @brief Get the number of frequency bins
-    // @return Number of frequency bins configured for this processor
+    /**
+     * @brief Get the number of frequency bins
+     * @return Number of frequency bins configured for this processor
+     */
     uint32_t getNumBins() const noexcept { return m_num_bins; }
 
-    // @brief Compute the complex FFT from audio samples
-    // @param samples Input audio samples (size must be equal to num_bins)
-    // @return Vector of complex FFT output (size will be num_bins / 2 + 1)
+    /**
+     * @brief Compute the complex FFT from audio samples
+     * @param samples Input audio samples (size must be equal to num_bins)
+     * @return Vector of complex FFT output (size will be num_bins / 2 + 1)
+     * @throws std::invalid_argument if samples.size() != num_bins
+     */
     std::vector<fftwf_complex> compute_complex(const std::vector<float>& samples);
 
-    // @brief Compute the frequency magnitudes from audio samples
-    // @param samples Input audio samples (size must be equal to num_bins)
-    // @return Vector of frequency magnitudes (size will be num_bins / 2 + 1)
+    /**
+     * @brief Compute the frequency magnitudes from audio samples
+     * @param samples Input audio samples (size must be equal to num_bins)
+     * @return Vector of frequency magnitudes (size will be num_bins / 2 + 1)
+     * @throws std::invalid_argument if samples.size() != num_bins
+     */
     std::vector<float> compute_magnitudes(const std::vector<float>& samples);
 
   private:

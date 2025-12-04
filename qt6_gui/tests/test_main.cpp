3fd7ef88bd3611899da0f9e98138c7ce1569a4f4
Chris Kai Frederick <git@neofin.net>
2025-12-03 18:10:20 -0800

    GUI test hello world

--- qt6_gui/tests/test_main.cpp
+++
@@ -1,7 +0,0 @@
-
-// This is just a placeholder until we add real tests.
-int
-main(int argc, char* argv[])
-{
-    return 0;
-}
---
+++ qt6_gui/tests/test_main_window.cpp
@@ -0,0 +1,44 @@
+#include "main_window.h"
+#include <QSignalSpy>
+#include <QTest>
+
+namespace spectro {
+
+class TestMainWindow : public QObject
+{
+    Q_OBJECT
+
+  private slots:
+    void testConstruction();
+    void testWindowTitle();
+    void testWindowSize();
+};
+
+void
+TestMainWindow::testConstruction()
+{
+    // Test that MainWindow can be constructed without crashing
+    MainWindow window;
+    QVERIFY(true);
+}
+
+void
+TestMainWindow::testWindowTitle()
+{
+    MainWindow window;
+    QCOMPARE(window.windowTitle(), QString("Spectro"));
+}
+
+void
+TestMainWindow::testWindowSize()
+{
+    MainWindow window;
+    QCOMPARE(window.width(), 800);
+    QCOMPARE(window.height(), 600);
+}
+
+} // namespace spectro
+
+// QTEST_MAIN creates a QApplication for widget tests
+QTEST_MAIN(spectro::TestMainWindow)
+#include "test_main_window.moc"

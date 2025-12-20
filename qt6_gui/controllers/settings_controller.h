0118c48513f87094f2082de4d5a91a2109b128b9
Chris Kai Frederick <git@neofin.net>
2025-12-20 14:47:35 -0800

    SettingsController initial commit

---
+++ qt6_gui/controllers/settings_controller.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "models/settings.h"
+#include <QAudioDevice>
+#include <QObject>
+
+/**
+ * @brief Controller for Settings
+ */
+class SettingsController : public QObject
+{
+    Q_OBJECT
+  public:
+    /**
+     * @brief Constructor
+     * @param aSettings Pointer to Settings model
+     * @param aParent Parent QObject
+     */
+    explicit SettingsController(Settings* aSettings, QObject* aParent = nullptr)
+      : QObject(aParent)
+      , mSettings(aSettings)
+    {
+    }
+
+    /**
+     * @brief Start audio recording with specified settings
+     * @param aDevice Audio device to use for recording
+     * @param aSampleRate Sample rate in Hz (e.g., 44100, 48000)
+     * @param aChannelCount Number of audio channels (e.g., 1 for mono, 2 for stereo)
+     * @throws std::invalid_argument if the sample rate or channel count is invalid
+     *
+     * Emits StartAudioRecording signal to notify AudioRecorder to start recording.
+     */
+    void StartRecording(const QAudioDevice& aDevice, int aSampleRate, int aChannelCount);
+
+  signals:
+
+    /**
+     * @brief Notify AudioRecorder to start audio recording with current settings
+     */
+    void StartAudioRecording();
+
+  private:
+    Settings* mSettings;
+};

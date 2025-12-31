fb31737fe8bb630e3f8c45ff7b02abe5233066f3
Chris Kai Frederick <git@neofin.net>
2025-12-31 14:46:18 -0800

    Move SampleRate type to dsp

--- qt6_gui/include/global_constants.h
+++ qt6_gui/include/global_constants.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <audio_types.h>
 #include <cstdint>
 
 using ChannelCount = uint8_t;
-using SampleRate = int;
 
 // Maximum number of audio channels supported by the application
 inline constexpr ChannelCount GKMaxChannels = 6;

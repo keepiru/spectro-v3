6d270a593b8532209b1ae23be482de4f66ba45e8
Chris Kai Frederick <git@neofin.net>
2026-01-12 12:35:25 -0800

    remove redundant ChannelCount def

--- qt6_gui/include/global_constants.h
+++ qt6_gui/include/global_constants.h
@@ -3,8 +3,6 @@
 #include <audio_types.h>
 #include <cstdint>
 
-using ChannelCount = uint8_t;
-
 // Maximum number of audio channels supported by the application
 inline constexpr ChannelCount GKMaxChannels = 6;
 
